22775dce5c77bbeae8acdbe54608342f50603f3b
yadaw-hash <tesfaytewelde2015@gmail.com>
2026-07-06 19:10:32 +0300

    fixing resume-navbar issues

--- app/routes/Resume.tsx
+++ app/routes/Resume.tsx
@@ -76,9 +76,9 @@ const Resume = () => {
         </Link>
       </nav>
       <div className='flex flex-row w-full max-lg:flex-col-reverse '>
-        <section className="feedback-section bg-[url('/images/bg-small.svg') bg-cover h-[calc(100vh-122px)] sticky top-122px items-center justify-center">
+        <section className="feedback-section bg-[url('/images/bg-small.svg') bg-cover h-screen sticky top-0 items-center justify-center">
           {imageUrl && resumeUrl && (
-            <div className='animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-2xl:h-fit w-fit'>
+            <div className='mt-50! animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-2xl:h-fit w-fit'>
               <a href={resumeUrl} target='_blank' rel='noopener noreferrer'>
                 <img
                   src={imageUrl}
